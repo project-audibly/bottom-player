7fefba785f1e7cbeb1881873dc5165294ea914a8
Michelle <mysdmg312@gmail.com>
2020-03-17 18:23:12 -0700

    Refactor file structure and environment

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import $ from "jquery";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  return() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+var bodyParser = require('body-parser');
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'audiblyBottomPlayer'
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
+
+app.use(express.static(path.join(__dirname, '../client/public')));
+
+app.get('/songs', (req, res) => {
+  connection.connect();
+  connection.query('SELECT * FROM songs', function(err, data) {
+    if (err) {
+      console.error('error getting data from db: ', err);
+    } else {
+      res.send(data);
+    }
+  });
+
+  connection.end();
+});
