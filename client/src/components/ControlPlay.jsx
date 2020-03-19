d53a199096728a0b3a96113d2c9f8a954a2f2b46
Michelle <mysdmg312@gmail.com>
2020-03-19 11:30:34 -0700

    Add conditional to play and pause current song

--- client/src/components/ControlPlay.jsx
+++ client/src/components/ControlPlay.jsx
@@ -3,42 +3,50 @@ import React from 'react';
 class ControlPlay extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isPlaying: false,
+    };
 
     this.handleClick = this.handleClick.bind(this);
   }
-  	// when the play button is clicked, play current song
-		
-		// does this class need a state? -- YES, play/pause
-		// where does the current song come from? App?
-		// need to render play button... where does play button come from? s3?
-		// need to render pause button
-		// first, render play button
-		// when play button is clicked, change button image to pause
-		// get current song's audio file from s3 and play
-		// when pause button is clicked, change button image to play
-    // stop playing current song
-    
-    handleClick() {
-      const audioElement = this.refs.song;
-      audioElement.play();
+
+  handleClick() {
+    const audioElement = this.refs.song;
+    if (this.state.isPlaying === false) {
+      this.setState({ isPlaying: true });
       document.getElementById('playbtn').innerHTML = 'CLICK ME TO PAUSE!';
+      audioElement.play();
+    } else {
+      this.setState({ isPlaying: false });
+      document.getElementById('playbtn').innerHTML = 'CLICK ME TO PLAY!';
+      audioElement.pause();
     }
+  }
 
-    render() {
-      return (
-        <div>
-          <button id="playbtn" onClick={this.handleClick}>CLICK ME TO PLAY!</button>
+  render() {
+    return (
+      <div>
+        <button id="playbtn" onClick={this.handleClick}>CLICK ME TO PLAY!</button>
         <audio
-        autoPlay={false}
-        ref="song"
-        src={this.props.song.song_audio}>
-            Your browser does not support the
-            <code>audio</code> element.
+          autoPlay={false}
+          ref="song"
+          src={this.props.song.song_audio}>
+          Your browser does not support the <code>audio</code> element.
         </audio>
-        </div>
-      )
-    }
-
+      </div>
+    );
+  }
 }
-
 export default ControlPlay;
+
+// when the play button is clicked, play current song
+
+// does this class need a state? -- YES, play/pause
+// where does the current song come from? App?
+// need to render play button... where does play button come from? s3?
+// need to render pause button
+// first, render play button
+// when play button is clicked, change button image to pause
+// get current song's audio file from s3 and play
+// when pause button is clicked, change button image to play
+// stop playing current song
