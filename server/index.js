d2419fc71892ec2350edf4c0f911a213bfe9dcf7
Michelle <mysdmg312@gmail.com>
2020-03-19 10:51:19 -0700

    Lint server code

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
+/* eslint no-console: ["error", { allow: ["log", "error"] }] */
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
 const port = 3000;
 const path = require('path');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'audiblyBottomPlayer'
+  database: 'audiblyBottomPlayer',
 });
 connection.connect();
 
@@ -18,8 +20,7 @@ app.listen(port, () => console.log(`Example app listening on port ${port}`));
 app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.get('/songs', (req, res) => {
-
-  connection.query('SELECT * FROM songs', function(err, data) {
+  connection.query('SELECT * FROM songs', (err, data) => {
     if (err) {
       console.error('error getting data from db: ', err);
     } else {
