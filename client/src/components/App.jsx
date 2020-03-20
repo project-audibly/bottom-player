f9046d723d689a5ae9cbf0dc0a18fc800a249d3d
Michelle <mysdmg312@gmail.com>
2020-03-19 17:42:21 -0700

    Add SongInfo component, refactor App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
-import ControlPlay from './ControlPlay.jsx';
 import $ from 'jquery';
+import ControlPlay from './ControlPlay.jsx';
+import SongInfo from './SongInfo.jsx';
+import Avatar from './Avatar.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       songs: [],
-      currentSong: []
+      currentSong: [],
     };
+
+    this.getSongs = this.getSongs.bind(this);
   }
 
   componentDidMount() {
+    this.getSongs();
+  }
+
+  getSongs() {
     $.get('/songs')
       .done((data) => {
         this.setState({ songs: data, currentSong: data[0] });
-    })
+      })
       .fail(() => {
         console.error('error getting songs');
       });
@@ -24,10 +32,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <ControlPlay song={this.state.currentSong}/>
+        <ControlPlay song={this.state.currentSong} />
+        <Avatar song={this.state.currentSong} />
+        <SongInfo song={this.state.currentSong} />
       </div>
     );
   }
 }
 
 export default App;
+
+// vvvv top of testing vvv
+// App.prototype.getSongs = something;
--- client/src/components/SongInfo.jsx
+++ client/src/components/SongInfo.jsx
@@ -1,3 +1,10 @@
-// subcomponents TBD:
-// - Title
-// - Artist
+import React from 'react';
+
+const SongInfo = (props) => (
+  <div>
+    <div>{props.song.artist_name}</div>
+    <div>{props.song.song_name}</div>
+  </div>
+);
+
+export default SongInfo;
